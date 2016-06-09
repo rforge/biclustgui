f112f1526312ab82b3d5f5e37b924b0316d1771b
ewoud <ewoud@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-06-09 12:22:57 +0000

    - update vignette + installation script
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biclustgui@73 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RcmdrPlugin.BiclustGUI/inst/doc/installation_script.R
+++ pkg/RcmdrPlugin.BiclustGUI/inst/doc/installation_script.R
@@ -1,3 +1,15 @@
+#############################################
+## AUTOMATIC INSTALLATION FOR CRAN RELEASE ##
+#############################################
+
+setRepositories(ind=c(1:5))
+install.packages("RcmdrPlugin.BiclustGUI")
+
+
+#################################################
+## MANUAL INSTALLATION FOR DEVELOPMENT RELEASE ##
+#################################################
+
 ## PACKAGES AVAILABLE ON CRAN ##
 install.packages("biclust")
 install.packages("BcDiag")
@@ -16,10 +28,19 @@ biocLite("fabia")
 biocLite("rqubic")
 biocLite("BicARE")
 
+## BiclustGUI - Release Version ##
+
+install.packages("RcmdrPlugin.BiclustGUI")
+
 ## Biclust GUI - In Development Version ##
 install.packages("RcmdrPlugin.BiclustGUI",
-repos="http://R-Forge.R-project.org")
+		repos="http://R-Forge.R-project.org")
+
+## Biclust GUI (ISA2 VERSION) - In Development Version ##
+install.packages("RcmdrPlugin.BiclustGUI.Extra",
+		repos="http://R-Forge.R-project.org")
 
 
+############################################################
 ## Note on initial launch of Rcmdr: 
 ## You might be prompted to isntall a few additional packages
